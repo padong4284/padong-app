0a93aa77cf437d4e720a219db58157c0c708eb20
Daewoong <kodw4284@gmail.com>
2021-02-07 22:19:49 +0900

    feat: Set project's convention with page routes

---
+++ lib/ui/views/loginView.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginView extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+class _LoginViewState extends State<LoginView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Login'))
+      ),
+      body: Container(
+        child: Text('Login Body')
+      )
+    );
+  }
+}

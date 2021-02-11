ce6ef74ba05a3559849dc2b311501912e13de5bb
Daewoong <kodw4284@gmail.com>
2021-02-12 00:26:23 +0900

    feat: Compose button wiget

---
+++ lib/ui/shared/button_properties.dart
@@ -0,0 +1,15 @@
+class ButtonProperties {
+
+  double height;
+  double width;
+  double padding;
+  double fontSize;
+  double roundedEdge;
+
+  ButtonProperties({ double height, double width, double padding = 0, double fontSize }) {
+    this.height = height;
+    this.width = width;
+    this.padding = padding;
+    this.fontSize = fontSize;
+  }
+}
--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
+import '../widgets/button.dart';
+import '../theme/app_theme.dart';
+import '../shared/types.dart';
 
 class LoginView extends StatefulWidget {
   @override
@@ -10,13 +13,46 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Center(child: Text('Login'))
-      ),
       body: Container(
-        child: Text('Login Body',
-            style: TextStyle(fontSize: AppTheme.fontSizes.giant),
-),
+        child: Column(
+          children: [
+              Container(
+                color: AppTheme.colors.primary,
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  margin: const EdgeInsets.only(top: 100.0, bottom: 140.0, left: 50.0),
+                  child: Text('PADONG', style: TextStyle(fontSize: AppTheme.fontSizes.giant, fontWeight: FontWeight.bold, color: AppTheme.colors.base)),
+                )
+              ),
+              Container(
+                color: AppTheme.colors.semiPrimary,
+                width: double.infinity,
+                child: Container(
+                  padding: EdgeInsets.only(top: 50.0, bottom: 90.0, right: 40.0),
+                  child: Column(
+                    children: [
+                      Container(
+                        alignment: Alignment.centerRight,
+                        width: double.infinity,
+                        child: Text('Welcome', style: TextStyle(fontSize: AppTheme.fontSizes.xlarge, fontWeight: FontWeight.bold, color: AppTheme.colors.support))
+                      ),
+                      Container(
+                        alignment: Alignment.centerRight,
+                        width: double.infinity,
+                        child: Text('Back', style: TextStyle(fontSize: AppTheme.fontSizes.xlarge, fontWeight: FontWeight.bold, color: AppTheme.colors.support)),
+                      )
+                    ]
+                  )
+                )
+              ),
+              Button(
+                  title: 'Sign Up',
+                  color: AppTheme.colors.primary,
+                  type: ButtonType.STADIUM,
+                  buttonSize: ButtonSize.SMALL,
+              )
+          ]
+        ),
       )
     );
   }

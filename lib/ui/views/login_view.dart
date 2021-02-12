48579c981c378148af5c20a5e4f12dbf6a1bbfce
JTJun <tae7130@snu.ac.kr>
2021-02-12 21:11:07 +0900

    feat: wave clip path

--- lib/ui/views/login_view.dart
+++ lib/ui/views/login_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../theme/app_theme.dart';
 import '../widgets/button.dart';
-import '../theme/app_theme.dart';
+import '../widgets/wave_clipper.dart';
 import '../shared/types.dart';
 
 class LoginView extends StatefulWidget {
@@ -10,25 +10,19 @@ class LoginView extends StatefulWidget {
 }
 
 class _LoginViewState extends State<LoginView> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Column(
-          children: [
-              Container(
-                color: AppTheme.colors.primary,
-                child: Container(
-                  alignment: Alignment.centerLeft,
-                  margin: const EdgeInsets.only(top: 100.0, bottom: 140.0, left: 50.0),
-                  child: Text('PADONG', style: TextStyle(fontSize: AppTheme.fontSizes.giant, fontWeight: FontWeight.bold, color: AppTheme.colors.base)),
-                )
-              ),
-              Container(
+        child: Stack(
+          children: <Widget>[
+            ClipPath(
+              child: Container(
                 color: AppTheme.colors.semiPrimary,
-                width: double.infinity,
+                height: 700,
                 child: Container(
-                  padding: EdgeInsets.only(top: 50.0, bottom: 90.0, right: 40.0),
+                  padding: EdgeInsets.only(top: 300.0, right: 40.0),
                   child: Column(
                     children: [
                       Container(
@@ -45,12 +39,26 @@ class _LoginViewState extends State<LoginView> {
                   )
                 )
               ),
-              Button(
-                  title: 'Sign Up',
+              clipper: SecondaryWaveClipper(),
+            ),
+            ClipPath(
+              child: Container(
                   color: AppTheme.colors.primary,
-                  type: ButtonType.STADIUM,
-                  buttonSize: ButtonSize.SMALL,
-              )
+                  height: 400,
+                  child: Container(
+                    alignment: Alignment.centerLeft,
+                    margin: const EdgeInsets.only(top: 0.0, left: 50.0),
+                    child: Text('PADONG', style: TextStyle(fontSize: AppTheme.fontSizes.giant, fontWeight: FontWeight.bold, color: AppTheme.colors.base)),
+                  )
+              ),
+              clipper: PrimaryWaveClipper(),
+            ),
+            Button(
+              title: 'Sign Up',
+              color: AppTheme.colors.primary,
+              type: ButtonType.STADIUM,
+              buttonSize: ButtonSize.SMALL,
+            )
           ]
         ),
       )

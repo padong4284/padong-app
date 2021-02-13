6ee32fdcb6a134667e61054d888c47cc673d9aa2
JTJun <tae7130@snu.ac.kr>
2021-02-13 19:52:04 +0900

    fix: can not access to input error

--- lib/ui/views/sign/sign_in_view.dart
+++ lib/ui/views/sign/sign_in_view.dart
@@ -14,25 +14,22 @@ class _SignInViewState extends State<SignInView> {
     return new SignView(
         true,
         "Welcome\nBack",
-        Hero(
-            tag: 'inputForm',
-            child: Center(
-                child: Container(
-                    width: 280,
-                    child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Container(
-                              margin: EdgeInsets.only(top: 550.0),
-                              height: 38.0,
-                              child: Input(
-                                  hintText: 'ID', type: InputType.ROUNDED)),
-                          Container(
-                              margin: EdgeInsets.only(top: 10.0),
-                              height: 38.0,
-                              child: Input(
-                                  hintText: 'Password',
-                                  type: InputType.ROUNDED))
-                        ])))));
+        Center(
+            child: Container(
+                width: 280,
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Container(
+                          margin: EdgeInsets.only(top: 550.0),
+                          height: 38.0,
+                          child:
+                              Input(hintText: 'ID', type: InputType.ROUNDED)),
+                      Container(
+                          margin: EdgeInsets.only(top: 10.0),
+                          height: 38.0,
+                          child: Input(
+                              hintText: 'Password', type: InputType.ROUNDED))
+                    ]))));
   }
 }

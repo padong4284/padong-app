813d7184e10b5612b134b536811b91771418325c
JTJun <tae7130@snu.ac.kr>
2021-02-13 12:28:00 +0900

    feat: dynamic starting wave

--- lib/ui/utils/wave/wave.dart
+++ lib/ui/utils/wave/wave.dart
@@ -1,4 +1,5 @@
 import "dart:math";
+
 var rand = new Random();
 
 class Point {
@@ -9,8 +10,8 @@ class Point {
   double speed = 0.05 + rand.nextInt(10) / 500;
 
   Point(amp, this.cur, this.yNorm) {
-    this.y = this.yNorm ;
     this.amplitude = rand.nextDouble() * 15 + amp;
+    this.y = this.yNorm + this.amplitude * sin(this.cur);
   }
 
   updating() {
@@ -27,17 +28,16 @@ class Wave {
   List<Point> points = [];
 
   Wave(this.amplitude, this.lean, this.yNorm, this.pointNum) {
-    for (int i=0; i<this.pointNum; i++) {
+    for (int i = 0; i < this.pointNum; i++) {
       this.points.add(new Point(
-        (i==0 || i==this.pointNum-1) ? 0 : this.amplitude,
-        i.toDouble(),
-        yNorm + lean * i)
-      );
+          (i == 0 || i == this.pointNum - 1) ? 0 : this.amplitude,
+          i.toDouble(),
+          yNorm + lean * i));
     }
   }
 
   updating() {
-    for (int i=1; i<this.pointNum-1; i++) {
+    for (int i = 1; i < this.pointNum - 1; i++) {
       this.points[i].updating();
     }
   }

8007353fb3dd3e22d4b5a0404dc0191f16b83729
Daewoong <kodw4284@gmail.com>
2021-02-13 19:18:21 +0900

    feat: Add main view route with rough frame
    * Add routes - p_main, main
    * Add views - p_main_view, main_view
    * Add bottom_navigation_bar.dart
    * Add a bar at top in both main and p_main view but it appears differently

---
+++ lib/ui/views/main/main_view.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:padong/ui/theme/app_theme.dart';
+import '../bottom_navigation_bar.dart';
+
+class MainView extends StatefulWidget {
+  final bool isPMain;
+
+  MainView({this.isPMain = false});
+
+  @override
+  _MainViewState createState() => _MainViewState();
+}
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+            child: Column(
+              children: [_buildTopBar()],
+            )),
+        bottomNavigationBar: PadongBottomNavigationBar());
+  }
+
+  Widget _buildTopBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Visibility(
+            child: Text('PADONG',
+                style: TextStyle(
+                    fontSize: AppTheme.fontSizes.large,
+                    color: AppTheme.colors.semiPrimary)),
+            visible: !widget.isPMain,
+        ),
+        Row(
+          children: [
+            SizedBox(
+                width: 32.0,
+                child: IconButton(
+                    icon: Icon(Icons.mode_comment,
+                        color: AppTheme.colors.support))),
+            SizedBox(
+                width: 32.0,
+                child: IconButton(
+                    icon: Icon(Icons.account_circle,
+                        color: AppTheme.colors.support)))
+          ],
+        )
+      ],
+    );
+  }
+}
---
+++ lib/ui/views/main/p_main_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'main_view.dart';
+
+class PMainView extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MainView(isPMain: true);
+  }
+}

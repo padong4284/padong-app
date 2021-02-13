0747c1a6482a19c12e46632143e2141d69523399
JTJun <tae7130@snu.ac.kr>
2021-02-13 10:30:36 +0900

    refactor: use SignView

--- lib/ui/router.dart
+++ lib/ui/router.dart
@@ -14,10 +14,6 @@ class PadongRouter {
             pageBuilder: (_, __ ,___ ) => SignUpView(),
             transitionsBuilder: (_, a, __, c) => FadeTransition(opacity: a, child: c)
         );
-      case '/sign_up':
-        return MaterialPageRoute(
-            builder: (_) => SignUpView()
-        );
       default:
         return MaterialPageRoute(
           builder: (_) => Scaffold(

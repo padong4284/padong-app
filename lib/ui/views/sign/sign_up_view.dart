84cc5c7d0749dda276f4c132aa6490610c9cfa99
JTJun <tae7130@snu.ac.kr>
2021-02-13 19:37:52 +0900

    feat: sign input forms

--- lib/ui/views/sign/sign_up_view.dart
+++ lib/ui/views/sign/sign_up_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'sign_view.dart';
+import '../../widgets/input.dart';
+import '../../shared/types.dart';
 
 class SignUpView extends StatefulWidget {
   @override
@@ -9,6 +11,56 @@ class SignUpView extends StatefulWidget {
 class _SignUpViewState extends State<SignUpView> {
   @override
   Widget build(BuildContext context) {
-    return new SignView(false, "Welcome");
+    return new SignView(
+        false,
+        "Welcome",
+        Hero(
+            tag: 'inputForm',
+            child: Center(
+                child: Container(
+                    width: 280,
+                    child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                              margin: EdgeInsets.only(top: 270.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'ID', type: InputType.ROUNDED)),
+                          Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'Password',
+                                  type: InputType.ROUNDED)),
+                          Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'Repeat Password',
+                                  type: InputType.ROUNDED)),
+                          Container(
+                              margin: EdgeInsets.only(top: 50.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'Name', type: InputType.ROUNDED)),
+                          Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'University',
+                                  type: InputType.ROUNDED)),
+                          Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'Entrance Year',
+                                  type: InputType.ROUNDED)),
+                          Container(
+                              margin: EdgeInsets.only(top: 10.0),
+                              height: 38.0,
+                              child: Input(
+                                  hintText: 'Email', type: InputType.ROUNDED))
+                        ])))));
   }
 }

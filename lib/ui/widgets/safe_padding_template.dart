ea13fd51dd8c8e15c3e55bc2a820eed65757fae7
JTJun <tae7130@snu.ac.kr>
2021-02-14 21:27:33 +0900

    feat: scrollabe safe padding template

--- lib/ui/widgets/safe_padding_template.dart
+++ lib/ui/widgets/safe_padding_template.dart
@@ -8,9 +8,9 @@ class SafePaddingTemplate extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Container(
+        child: SingleChildScrollView(
       padding: EdgeInsets.symmetric(horizontal: 25.0),
-      child: this.child,
+      child: (this.child),
     ));
   }
 }

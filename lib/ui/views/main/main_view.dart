7f8f0c73edadd7a2ad51a200ae4fa364eb2dbdb5
Daewoong <kodw4284@gmail.com>
2021-02-14 15:38:50 +0900

    feat: Add SafeArea to MainView

--- lib/ui/views/main/main_view.dart
+++ lib/ui/views/main/main_view.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/theme/app_theme.dart';
-import 'package:padong/ui/views/bottom_navigation_bar.dart';
 import 'package:padong/ui/widgets/post_card.dart';
 import 'package:padong/ui/widgets/horizontal_scroller.dart';
+import 'package:padong/ui/widgets/bottom_navigation_bar.dart';
 
 class MainView extends StatefulWidget {
   final bool isPMain;
@@ -17,19 +17,21 @@ class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Container(
-            padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-            child: Column(
-              children: [
-                this._buildTopBar(),
-                HorizontalScroller(
-                  padding: 3.0,
-                  parentLeftPadding: 20.0,
-                    parentRightPadding: 20.0,
-                  children: Iterable<int>.generate(10).map((idx) => PostCard(idx.toString())).toList()
-                )
-              ],
-            )),
+        body: SafeArea(
+            child: Container(
+                padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
+                child: Column(
+                  children: [
+                    this._buildTopBar(),
+                    HorizontalScroller(
+                        padding: 3.0,
+                        parentLeftPadding: 20.0,
+                        parentRightPadding: 20.0,
+                        children: Iterable<int>.generate(10)
+                            .map((idx) => PostCard(idx.toString()))
+                            .toList())
+                  ],
+                ))),
         bottomNavigationBar: PadongBottomNavigationBar());
   }
 
@@ -38,11 +40,11 @@ class _MainViewState extends State<MainView> {
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Visibility(
-            child: Text('PADONG',
-                style: TextStyle(
-                    fontSize: AppTheme.fontSizes.large,
-                    color: AppTheme.colors.semiPrimary)),
-            visible: !widget.isPMain,
+          child: Text('PADONG',
+              style: TextStyle(
+                  fontSize: AppTheme.fontSizes.large,
+                  color: AppTheme.colors.semiPrimary)),
+          visible: !widget.isPMain,
         ),
         Row(
           children: [

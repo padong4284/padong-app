f052d0d344f258d60d861445476457ce6ed9bc47
Daewoong <kodw4284@gmail.com>
2021-02-14 16:08:21 +0900

    feat: Implement topbar by using AppBar

--- lib/ui/views/main/main_view.dart
+++ lib/ui/views/main/main_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:padong/ui/theme/app_theme.dart';
 import 'package:padong/ui/widgets/post_card.dart';
 import 'package:padong/ui/widgets/horizontal_scroller.dart';
-import 'package:padong/ui/widgets/bottom_navigation_bar.dart';
 
 class MainView extends StatefulWidget {
   final bool isPMain;
@@ -17,12 +16,12 @@ class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        appBar: _buildTopBar(),
         body: SafeArea(
             child: Container(
                 padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
                 child: Column(
                   children: [
-                    this._buildTopBar(),
                     HorizontalScroller(
                         padding: 3.0,
                         parentLeftPadding: 20.0,
@@ -31,35 +30,37 @@ class _MainViewState extends State<MainView> {
                             .map((idx) => PostCard(idx.toString()))
                             .toList())
                   ],
-                ))),
-        bottomNavigationBar: PadongBottomNavigationBar());
+                ))));
   }
 
-  Widget _buildTopBar() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Visibility(
+  AppBar _buildTopBar() {
+    return AppBar(
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      leading: Visibility(
+        child: Container(
+          padding: EdgeInsets.only(left: 25.0),
+          alignment: Alignment.center,
           child: Text('PADONG',
               style: TextStyle(
                   fontSize: AppTheme.fontSizes.large,
                   color: AppTheme.colors.semiPrimary)),
-          visible: !widget.isPMain,
         ),
-        Row(
-          children: [
-            SizedBox(
-                width: 32.0,
-                child: IconButton(
-                    icon: Icon(Icons.mode_comment,
-                        color: AppTheme.colors.support))),
-            SizedBox(
-                width: 32.0,
-                child: IconButton(
-                    icon: Icon(Icons.account_circle,
-                        color: AppTheme.colors.support)))
-          ],
-        )
+        visible: !widget.isPMain,
+      ),
+      leadingWidth: 110.0,
+      actions: [
+        SizedBox(
+            width: 32.0,
+            child: IconButton(
+                icon:
+                    Icon(Icons.mode_comment, color: AppTheme.colors.support))),
+        SizedBox(
+            width: 32.0,
+            child: IconButton(
+                icon: Icon(Icons.account_circle,
+                    color: AppTheme.colors.support))),
+        SizedBox(width: 25.0)
       ],
     );
   }

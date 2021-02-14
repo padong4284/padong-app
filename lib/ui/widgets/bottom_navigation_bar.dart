9e1d7db0e1efbd611f774308bd702d5b0d64d436
Daewoong <kodw4284@gmail.com>
2021-02-14 18:21:51 +0900

    feat: change BottomNavigationBar to stateful widget

--- lib/ui/widgets/bottom_navigation_bar.dart
+++ lib/ui/widgets/bottom_navigation_bar.dart
@@ -1,15 +1,40 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/theme/app_theme.dart';
 
-class PadongBottomNavigationBar extends StatelessWidget {
+class PadongBottomNavigationBar extends StatefulWidget {
   final color = AppTheme.colors.semiSupport;
   final size = 40.0;
+
+  PadongBottomNavigationBar();
+
+  @override
+  _PadongBottomNavigationBarState createState() => _PadongBottomNavigationBarState();
+}
+
+class _PadongBottomNavigationBarState extends State<PadongBottomNavigationBar> {
+
+  int _selectedIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     const iconWidth = 57.0;
-
     return BottomNavigationBar(
+      currentIndex: this._selectedIndex ?? 0,
+      selectedItemColor: AppTheme.colors.primary,
+        unselectedItemColor: widget.color,
+        onTap: (index) {
+          if (index != _selectedIndex) {
+            _buildRoutes(index, context);
+          }
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
         showSelectedLabels: false,
         showUnselectedLabels: false,
         type: BottomNavigationBarType.fixed,
@@ -19,19 +44,19 @@ class PadongBottomNavigationBar extends StatelessWidget {
             icon: SizedBox(
                 width: iconWidth,
                 child: Icon(Icons.home_filled,
-                    color: this.color, size: this.size)),
+                     size: widget.size)),
           ),
           BottomNavigationBarItem(
               label: 'Cover',
               icon: SizedBox(
                 width: iconWidth,
-                child: Icon(Icons.book, color: this.color, size: this.size),
+                child: Icon(Icons.book, size: widget.size),
               )),
           BottomNavigationBarItem(
               label: 'Deck',
               icon: SizedBox(
                 width: iconWidth,
-                child: Icon(Icons.wysiwyg, color: this.color, size: this.size),
+                child: Icon(Icons.wysiwyg, size: widget.size),
               )),
           BottomNavigationBarItem(
             label: 'Schedule',
@@ -39,8 +64,7 @@ class PadongBottomNavigationBar extends StatelessWidget {
                 width: iconWidth,
                 child: Icon(
                   Icons.event,
-                  color: this.color,
-                  size: this.size,
+                  size: widget.size,
                 )),
           ),
           BottomNavigationBarItem(
@@ -49,10 +73,30 @@ class PadongBottomNavigationBar extends StatelessWidget {
                 width: iconWidth,
                 child: Icon(
                   Icons.place,
-                  color: this.color,
-                  size: this.size,
+                  color: widget.color,
+                  size: widget.size,
                 )),
           ),
         ]);
+  }
+
+  Function _buildRoutes(int index, BuildContext context) {
+    switch (index) {
+      case 0:
+        Navigator.pushNamed(context, '/main');
+        break;
+      case 1:
+        Navigator.pushNamed(context, '/cover');
+        break;
+      case 2:
+        Navigator.pushNamed(context, '/deck');
+        break;
+      case 3:
+        Navigator.pushNamed(context, '/schedule');
+        break;
+      case 4:
+        Navigator.pushNamed(context, '/map');
+        break;
+    }
   }
 }

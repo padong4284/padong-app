7372d04bcd775e64ad2074a00d64e287888baeda
Daewoong <kodw4284@gmail.com>
2021-02-15 19:35:52 +0900

    feat: Add views which have bottom navigation bar

---
+++ lib/ui/views/lecture/lecture_list_view.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LectureListView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Lecture List View');
+  }
+}

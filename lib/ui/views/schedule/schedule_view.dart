697703363e8f9175475b27ba7d7127f86dae1eb9
JTJun <tae7130@snu.ac.kr>
2021-02-16 23:44:26 +0900

    refactor: separate EventCard and LectureCard

--- lib/ui/views/schedule/schedule_view.dart
+++ lib/ui/views/schedule/schedule_view.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/widgets/safe_padding_template.dart';
+import 'package:padong/ui/widgets/containers/vertical_timeline.dart';
+import 'package:padong/ui/widgets/cards/event_card.dart';
 
 class ScheduleView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafePaddingTemplate(
-      children: [Text('Schedule View')],
+      children: [
+        VerticalTimeline(
+            indexs: Iterable<int>.generate(10)
+                .map((idx) => idx.toString())
+                .toList(),
+            children:
+                Iterable<int>.generate(10).map((idx) => EventCard('1234')).toList())
+      ],
     );
   }
 }

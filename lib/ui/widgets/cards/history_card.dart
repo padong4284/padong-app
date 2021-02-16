309b1804b35cd70a4556d3c10aec22d5ab588f84
JTJun <tae7130@snu.ac.kr>
2021-02-17 03:42:30 +0900

    feat: vertical timeline with history tile

--- lib/ui/widgets/cards/history_card.dart
+++ lib/ui/widgets/cards/history_card.dart
@@ -17,18 +17,63 @@ Map<String, dynamic> getEvent(String id) {
 }
 
 class HistoryCard extends NodeBase {
+  final String historyId;
   final Map<String, dynamic> event;
 
   HistoryCard(id)
-      : this.event = getEvent(id), super(id);
+      : this.event = getEvent(id),
+        historyId = id,
+        // TODO: get history id by id
+        super(id);
 
   @override
   Widget build(BuildContext context) {
-    return BaseCard(children: [ super.build(context) ]);
+    return BaseCard(children: [super.build(context)]);
+  }
+
+  @override
+  Widget commonArea() {
+    this.node['time'] = '13:27:04'; // TODO:  HH:MM:SS timestamp
+    return super.commonArea();
   }
 
   @override
   Widget bottomArea() {
-    return SizedBox(height: 10);
+    return Container(
+        padding: const EdgeInsets.only(top: 2),
+        transform: Matrix4.translationValues(0.0, 5.0, 0.0),
+        child: Stack(
+          children: [
+            Row(
+              children: [
+                Icon(Icons.add_rounded,
+                    size: 15, color: AppTheme.colors.primary),
+                Container(
+                    width: 30,
+                    child: Text('15', // TODO: get difference ADDED
+                        style: AppTheme.getFont(
+                            color: AppTheme.colors.primary,
+                            fontSize: AppTheme.fontSizes.small))),
+                Icon(Icons.remove_rounded,
+                    size: 15, color: AppTheme.colors.pointRed),
+                SizedBox(
+                    width: 30,
+                    child: Text('15', // TODO: get difference REMOVED
+                        style: AppTheme.getFont(
+                            color: AppTheme.colors.pointRed,
+                            fontSize: AppTheme.fontSizes.small)))
+              ],
+            ),
+            Positioned(
+                bottom: 2,
+                right: 0,
+                child: Text(
+                  'e0434' + this.historyId, // TODO: history ID
+                  style: AppTheme.getFont(
+                      color: AppTheme.colors.fontPalette[2],
+                      fontSize: AppTheme.fontSizes.small),
+                ))
+          ],
+        ));
   }
 }

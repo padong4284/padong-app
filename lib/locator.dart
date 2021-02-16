8b63fc0217bc864b3286db51c88e3cd7e1be88ff
Seongbin Hong <vertextoedge@gmail.com>
2021-02-16 22:17:23 +0900

    refactor: Delete viewmodels

--- lib/locator.dart
+++ lib/locator.dart
@@ -2,17 +2,9 @@
 import 'package:get_it/get_it.dart';
 
 import 'core/services/firestore_api.dart';
-import 'core/viewmodels/deck/deck.dart';
 
 GetIt locator = GetIt();
 
 void setupLocator() {
   locator.registerLazySingleton(() => FirestoreAPI('deck'), "Firestore:deck");
-  locator.registerLazySingleton(() => Deck()) ;
-  locator.registerLazySingleton(() => FirestoreAPI('board'), "Firestore:board");
-  locator.registerLazySingleton(() => FirestoreAPI('post'), "Firestore:post");
-  locator.registerLazySingleton(() => FirestoreAPI('reply'), "Firestore:reply");
-  locator.registerLazySingleton(() => FirestoreAPI('rereply'), "Firestore:rereply");
-
-
 }

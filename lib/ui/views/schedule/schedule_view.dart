3bf1d8b80a971a0b1cd3da10960d4cd97987dc34
JTJun <tae7130@snu.ac.kr>
2021-02-17 02:51:10 +0900

    feat: define api of vertical timeline

--- lib/ui/views/schedule/schedule_view.dart
+++ lib/ui/views/schedule/schedule_view.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/widgets/safe_padding_template.dart';
 import 'package:padong/ui/widgets/containers/vertical_timeline.dart';
+import 'package:padong/ui/widgets/cards/time_card.dart';
 
 class ScheduleView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafePaddingTemplate(
       children: [
-        VerticalTimeline(
-            date: '04/02/2021',
-            ids: Iterable<int>.generate(10)
-                .map((idx) => idx.toString())
-                .toList())
+        VerticalTimeline(date: '04/02/2021', dots: [
+          '09:15',
+          '10:30',
+          '11:45'
+        ], cards: [
+          [TimeCard('1')],
+          [TimeCard('2'), TimeCard('3')],
+          [TimeCard('4'), TimeCard('5'), TimeCard('6')]
+        ])
       ],
     );
   }
--- lib/ui/widgets/containers/vertical_timeline.dart
+++ lib/ui/widgets/containers/vertical_timeline.dart
@@ -1,20 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/theme/app_theme.dart';
-import 'package:padong/ui/widgets/cards/time_card.dart';
 
 class VerticalTimeline extends StatelessWidget {
   final String date; // TODO: DateTime class
-  final List<String> ids;
-  final bool hideHead;
+  final List<String> dots;
+  final List<List<Widget>> cards;
+  final bool hideTopDate;
 
-  VerticalTimeline({@required this.date, @required this.ids, this.hideHead=false});
+  VerticalTimeline(
+      {@required this.date,
+      @required List<String> dots,
+      @required List<List<Widget>> cards,
+      this.hideTopDate = false})
+      : assert(dots.length == cards.length),
+        this.dots = dots,
+        this.cards = cards;
 
   @override
   Widget build(BuildContext context) {
-    int len = this.ids.length;
+    int len = this.dots.length;
     return Stack(children: [
       Positioned.fill(
-          top: this.hideHead ? 15 : 35,
+          top: this.hideTopDate ? 15 : 35,
           left: 4,
           right: MediaQuery.of(context).size.width -
               2 * (AppTheme.horizontalPadding + 4),
@@ -23,43 +30,41 @@ class VerticalTimeline extends StatelessWidget {
       Padding(
           padding: const EdgeInsets.only(bottom: 20),
           child: Column(children: [
-            this.hideHead ? SizedBox() : this.getHead(this.date),
+            this.hideTopDate ? SizedBox() : this.getTopDate(),
             ...Iterable<int>.generate(len).map((idx) => Column(children: [
-                  this.getDotTime(this.ids[idx]),
+                  this.getDotTime(this.dots[idx]),
                   Padding(
                       padding: const EdgeInsets.only(left: 15),
-                      child: Column(children: [
-                        TimeCard(this.ids[idx]),
-                        TimeCard(this.ids[idx])
-                      ]))
+                      child: Column(children: this.cards[idx]))
                 ]))
           ]))
     ]);
   }
 
-  Widget getHead(String date, {bool isToday = false}) {
+  Widget getTopDate() {
+    bool isToday = false; // TODO check this.head is today
     return Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                isToday ? 'Today' : this.date,
-                style: AppTheme.getFont(
-                    color: AppTheme.colors.support,
-                    fontSize: AppTheme.fontSizes.mlarge,
-                    isBold: true),
-              ),
-              isToday
-                  ? Text(this.date,
-                      style: AppTheme.getFont(
-                          color: AppTheme.colors.semiSupport,
-                          fontSize: AppTheme.fontSizes.regular))
-                  : null
-            ].where((elm) => elm != null).toList());
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            isToday ? 'Today' : this.date,
+            style: AppTheme.getFont(
+                color: AppTheme.colors.support,
+                fontSize: AppTheme.fontSizes.mlarge,
+                isBold: true),
+          ),
+          isToday
+              ? Text(this.date,
+                  style: AppTheme.getFont(
+                      color: AppTheme.colors.semiSupport,
+                      fontSize: AppTheme.fontSizes.regular))
+              : null
+        ].where((elm) => elm != null).toList());
   }
 
-  Widget getDotTime(String id) {
+  Widget getDotTime(String dotTime) {
     return Padding(
-        padding: const EdgeInsets.only(top:10, bottom: 3),
+        padding: const EdgeInsets.only(top: 10),
         child: Row(
           children: [
             ClipRRect(
@@ -69,7 +74,7 @@ class VerticalTimeline extends StatelessWidget {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 10),
-              child: Text('12:0' + id, // TODO: get time by id
+              child: Text(dotTime,
                   style: AppTheme.getFont(
                       color: AppTheme.colors.semiSupport,
                       fontSize: AppTheme.fontSizes.regular)),

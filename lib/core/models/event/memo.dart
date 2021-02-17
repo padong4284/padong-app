a6ae012c6538275deb54d243dc4b82e5431d0614
Seongbin Hong <vertextoedge@gmail.com>
2021-02-17 20:27:39 +0900

    fix: Fix wrong class name in ModelMemo

--- lib/core/models/event/memo.dart
+++ lib/core/models/event/memo.dart
@@ -8,10 +8,12 @@ class ModelEvent extends ModelPost {
   ModelEvent({
     id,
     title, description,
+    attachments, anonymity,
     parentNodeId, ownerId, pip,
     createdAt, deletedAt, modifiedAt}):
         super(id: id,
           title:title, description: description,
+          attachments: attachments, anonymity: anonymity,
           parentNodeId: parentNodeId, ownerId: ownerId, pip: pip,
           createdAt: createdAt, deletedAt: deletedAt, modifiedAt: modifiedAt);
 

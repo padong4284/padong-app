9e97cb0d23084339529dd619f91287e078b413b0
Daewoong Ko <kodw4284@gmail.com>
2021-02-18 00:02:14 +0900

    feat: compose friend tile (#104)
    * refactor: Relocate bottom_navigation_bar.dart
    
    * feat: Add LeftRightPaddingContainer which gives padding at left and right
    
    * feat: Add padding to each icon in bottom navigation bar
    
    * feat: change BottomNavigationBar to stateful widget
    
    * feat: Add views which have bottom navigation bar
    
    * refactor: Hoist selectedIndex state to main view
    
    * feat: Implement routing function in buttom navigation bar
    
    * misc: Remove useless codes
    
    * feat: Compose user profile button
    
    * feat: Compose Friend Tile

--- lib/ui/views/deck/deck_view.dart
+++ lib/ui/views/deck/deck_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:padong/ui/shared/types.dart';
 import 'package:padong/ui/widgets/safe_padding_template.dart';
 import 'package:padong/ui/widgets/buttons/user_profile_button.dart';
+import 'package:padong/ui/widgets/tiles/friend_tile.dart';
 
 class DeckView extends StatelessWidget {
   @override
@@ -10,7 +11,8 @@ class DeckView extends StatelessWidget {
       children: [
         Text('Deck View'),
         UserProfileButton(username: 'kod0402', position: UsernamePosition.RIGHT_CENTER),
-        UserProfileButton(username: 'kod0402', position: UsernamePosition.BOTTOM)
+        UserProfileButton(username: 'kod0402', position: UsernamePosition.BOTTOM),
+        FriendTile(username: 'tae7130',universityName: 'Seoul Nat\'l', enteranceYear: '2018', isVerified: true,)
       ],
     );
   }

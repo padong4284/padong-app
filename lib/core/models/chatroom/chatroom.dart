3ee92643ae08516c692fc19bd07f73b1ad4ed7dd
Seongbin Hong <vertextoedge@gmail.com>
2021-02-17 20:31:09 +0900

    fix: Fix class name's CamelCase

--- lib/core/models/chatroom/chatroom.dart
+++ lib/core/models/chatroom/chatroom.dart
@@ -4,16 +4,16 @@ import '../title_node.dart';
 /*
 * ModelChatRoom has no parent
 * */
-class ModelChatRoom extends ModelTitleNode {
+class ModelChatroom extends ModelTitleNode {
 
-  ModelChatRoom({id, title, description, parentNodeId, ownerId, pip, createdAt, deletedAt,
+  ModelChatroom({id, title, description, parentNodeId, ownerId, pip, createdAt, deletedAt,
     modifiedAt}):
         super(id: id,
           title:title, description: description,
           parentNodeId: parentNodeId, ownerId: ownerId, pip: pip,
           createdAt: createdAt, deletedAt: deletedAt, modifiedAt: modifiedAt);
 
-  ModelChatRoom.fromMap(Map snapshot,String id) :
+  ModelChatroom.fromMap(Map snapshot,String id) :
         super.fromMap(snapshot, id);
 
   toJson() {

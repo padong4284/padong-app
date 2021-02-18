ea639e6a9c3a81cc18a2b6f40ae0fbbaf4dadc50
JTJun <35912840+jtjun@users.noreply.github.com>
2021-02-19 00:42:55 +0900

    feat: Top Back App Bar (#111)
    * feat: top back app bar
    
    * feat: ToggleIconButton
    
    * refactor: rename bottom_navigation_bar -> padong_bottom_navigation_bar
    
    * refactor: BottomButtons using ToggleIconButton
    
    * refactor: rename title_bar -> title_header
    
    * feat: signle button action

--- lib/ui/views/sign/forgot_view.dart
+++ lib/ui/views/sign/forgot_view.dart
@@ -1,13 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/shared/types.dart';
+import 'package:padong/ui/shared/dialog_callback.dart';
+import 'package:padong/ui/theme/app_theme.dart';
+import 'package:padong/ui/widgets/buttons/button.dart';
+import 'package:padong/ui/widgets/buttons/switch_button.dart';
+import 'package:padong/ui/widgets/buttons/toggle_icon_button.dart';
+import 'package:padong/ui/widgets/cards/lecture_card.dart';
 import 'package:padong/ui/widgets/safe_padding_template.dart';
 import 'package:padong/ui/widgets/inputs/bottom_sender.dart';
+import 'package:padong/ui/widgets/bars/back_app_bar.dart';
 
 class ForgotView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafePaddingTemplate(
-      children: [],
+      appBar: BackAppBar(
+          switchButton: SwitchButton(
+            options: ['write', 'prev'],
+          ),
+          actions: [
+            Button(
+                title: 'OK', shadow: false,
+                buttonSize: ButtonSize.SMALL,
+                borderColor: AppTheme.colors.primary)
+            /*
+        ToggleIconButton(
+            defaultIcon: Icons.favorite_outline_rounded,
+            toggleIcon: Icons.favorite_rounded,
+            toggleColor: AppTheme.colors.pointRed,
+            isToggled: true),
+        ToggleIconButton(
+          defaultIcon: Icons.bookmark_outline_rounded,
+          toggleIcon: Icons.bookmark_rounded,
+          onPressed:
+              dialogCallback(context, 'Bookmark', 'Add page to bookmarks!'),
+        )*/
+          ]),
+      children: [
+        LectureCard('1234'),
+        LectureCard('1234'),
+        LectureCard('1234'),
+        LectureCard('1234'),
+        LectureCard('1234'),
+        LectureCard('1234')
+      ],
       floatingBottomBar: BottomSender(BottomSenderType.CHAT),
     );
   }
---
+++ lib/ui/widgets/bars/back_app_bar.dart
@@ -0,0 +1,61 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:padong/ui/shared/types.dart';
+import 'package:padong/ui/theme/app_theme.dart';
+import 'package:padong/ui/widgets/buttons/button.dart';
+import 'package:padong/ui/widgets/buttons/switch_button.dart';
+import 'package:padong/ui/widgets/buttons/transp_button.dart';
+
+class BackAppBar extends StatelessWidget implements PreferredSizeWidget {
+  @override
+  final Size preferredSize; // default is 56.0
+  final String title;
+  final List<Widget> actions;
+  final SwitchButton switchButton;
+  final bool isClose;
+
+  BackAppBar({Key key, title, switchButton, this.actions, this.isClose = false})
+      : preferredSize = Size.fromHeight(kToolbarHeight),
+        assert(title == null || switchButton == null),
+        this.title = title,
+        this.switchButton = switchButton,
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      brightness: Brightness.light,
+      // when dark mode, using dark
+      backgroundColor: Colors.transparent,
+      elevation: 0,
+      title: this.switchButton != null
+          ? this.switchButton
+          : this.title != null
+              ? Text(this.title,
+                  textAlign: TextAlign.center,
+                  style: AppTheme.getFont(
+                      color: AppTheme.colors.fontPalette[0],
+                      fontSize: AppTheme.fontSizes.large))
+              : null,
+      centerTitle: true,
+      leading: Padding(
+          padding: EdgeInsets.only(left: AppTheme.horizontalPadding),
+          child: TranspButton(
+              buttonSize: ButtonSize.LARGE,
+              callback: () {
+                Navigator.pop(context);
+              },
+              icon: Icon(
+                  this.isClose ? Icons.close : Icons.arrow_back_ios_rounded,
+                  size: 25))),
+      leadingWidth: 25 + AppTheme.horizontalPadding,
+      actions: [
+        ...(this.actions ?? []).map((button) => Container(
+            alignment: Alignment.centerRight,
+            child: SizedBox(width: button is Button ? 67 : 32, child: button))),
+        SizedBox(width: AppTheme.horizontalPadding)
+      ],
+    );
+  }
+}

f56bdbed512d3ad968a9c28fab8ead3dd22735ee
JTJun <tae7130@snu.ac.kr>
2021-02-19 03:45:50 +0900

    feat: title line

--- lib/ui/widgets/inputs/list_picker.dart
+++ lib/ui/widgets/inputs/list_picker.dart
@@ -7,13 +7,16 @@ class ListPicker extends StatelessWidget {
   final String hintText;
   final List<List> lists;
   final Function onChanged;
+  final List<String> titles;
   final controller = TextEditingController();
 
-  ListPicker({this.hintText, @required List list, this.onChanged})
+  ListPicker({this.hintText, @required List list, this.onChanged, String title})
       : assert(list != null && list.length > 0),
-        this.lists = <List>[list];
+        this.lists = <List>[list],
+        this.titles = [title];
 
-  ListPicker.multiple({this.hintText, @required this.lists, this.onChanged});
+  ListPicker.multiple(
+      {this.hintText, @required this.lists, this.onChanged, this.titles});
 
   @override
   Widget build(BuildContext context) {
@@ -37,23 +40,50 @@ class ListPicker extends StatelessWidget {
   void _showPicker(BuildContext context) {
     showCupertinoModalPopup(
         context: context,
-        builder: (_) => Container(
-            height: 250,
-            color: AppTheme.colors.base,
-            padding: const EdgeInsets.symmetric(horizontal: AppTheme.horizontalPadding),
-            child: Row(children:
-              this.lists.map((list) =>
-              Expanded(
-                  child: CupertinoPicker(
-                itemExtent: 30,
-                scrollController: FixedExtentScrollController(initialItem: -1),
-                children: list.map((elm) => Text(elm.toString())).toList(),
-                onSelectedItemChanged: (idx) {
-                  // selected idx
-                  if (this.onChanged != null) this.onChanged(idx);
-                  this.controller.text = list[idx].toString();
-                },
-              ))).toList(), // Add other Lists
-            )));
+        builder: (_) => Stack(children: [
+              Container(
+                  height: 250,
+                  color: AppTheme.colors.base,
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: AppTheme.horizontalPadding),
+                  child: Row(
+                    children: this
+                        .lists
+                        .map((list) => Expanded(
+                                child: CupertinoPicker(
+                              itemExtent: 30,
+                              scrollController:
+                                  FixedExtentScrollController(initialItem: -1),
+                              children: list
+                                  .map((elm) => Text(elm.toString()))
+                                  .toList(),
+                              onSelectedItemChanged: (idx) {
+                                // selected idx
+                                if (this.onChanged != null) this.onChanged(idx);
+                                this.controller.text = list[idx].toString();
+                              },
+                            )))
+                        .toList(), // Add other Lists
+                  )),
+              Container(
+                  height: 50,
+                  color: AppTheme.colors.base,
+                  padding: const EdgeInsets.only(
+                      top: 15,
+                      left: AppTheme.horizontalPadding,
+                      right: AppTheme.horizontalPadding),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: this
+                          .titles
+                          .where((title) => title != null)
+                          .map((title) => Text(
+                                title,
+                                style: AppTheme.getFont(
+                                    color: AppTheme.colors.primary,
+                                    fontSize: AppTheme.fontSizes.large),
+                              ))
+                          .toList()))
+            ]));
   }
 }

7bf94e7d4a73d8832d98e1c0af777ec85dd4ae8a
Seongbin Hong <vertextoedge@gmail.com>
2021-02-19 00:08:11 +0900

    feat: Add getFriends to User in viewmodels User

--- lib/core/viewmodels/user/user.dart
+++ lib/core/viewmodels/user/user.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:padong/core/models/user/user.dart';
 import 'package:padong/core/services/firestore_api.dart';
-
 import 'package:padong/locator.dart';
 
 
@@ -29,6 +28,7 @@ class User extends ModelUser {
   User.fromMap(Map snapshot,String id) :
         super.fromMap(snapshot, id);
 
+  ///getUserById return User instance
   static Future<User> getUserById(String id) async {
     DocumentSnapshot docUser = await _userDB.ref.doc(id).get();
     if (docUser.exists){
@@ -37,5 +37,34 @@ class User extends ModelUser {
     throw Exception("UserId doesn't exists");
   }
 
+  /// getFreinds return List<User>
+  Future<List<User>> getFriends() async{
+    List<User> friendList;
+
+    //Maybe it will be refactored later.
+    for (String friendId in this.friendIds) {
+      QuerySnapshot query = await _userDB.ref.where(
+          "id", isEqualTo: friendId).get();
+      if (query.size == 0) {
+        continue;
+      }
+      friendList.add(
+          User.fromMap(query.docs.first.data(), query.docs.first.id));
+    }
+    return friendList;
+  }
+
+  /// getParent return ModelUniversity
+  //todo : erase remark when Uiversity_view_model is done.
+  /*Future<ModelUniversity> getParent(){
+    University university;
+    return university.getUniversityById(this.parentNodeId);
+  }*/
+
+  ///getPosts return List<Post>
+  //todo : erase remark when Post_view_model is done.
+  /*List<Post> getPosts() {
+    return Post.getPostsById(this.id);
+  }*/
 }
 

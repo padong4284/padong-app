3c8d8a1aa756b36f0fa6288e591b03c72c1fb8c1
JTJun <tae7130@snu.ac.kr>
2021-02-19 22:20:43 +0900

    feat: day time range picker

--- lib/ui/widgets/inputs/times/day_time_range.dart
+++ lib/ui/widgets/inputs/times/day_time_range.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/widgets/inputs/list_picker.dart';
-import 'package:padong/ui/widgets/inputs/time_list_picker.dart';
+import 'package:padong/ui/widgets/inputs/times/time_range_picker.dart';
 
-class TimeRangePicker extends TimeListPicker {
+class DayTimeRangePicker extends TimeRangePicker {
   final String hintText;
   final int minuteGap;
   final int initStartHour;
   final int initFinishHour;
 
-  TimeRangePicker(
-      {this.hintText = 'Start ~ Finish',
+  DayTimeRangePicker(
+      {this.hintText = 'Day | Start ~ Finish',
       this.minuteGap = 1,
       this.initStartHour = 0,
       this.initFinishHour = 23})
@@ -19,16 +19,13 @@ class TimeRangePicker extends TimeListPicker {
   Widget build(BuildContext context) {
     return ListPicker.multiple(
       hintText: this.hintText,
-      lists: [...this.getHourNMinute(), ['~'], ...this.getHourNMinute()],
-      initIdxs: [
-        this.initStartHour,
-        30 ~/ this.minuteGap,
-        0,
-        this.initFinishHour,
-        30 ~/ this.minuteGap
+      lists: [
+        ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
+        ...this.getTimeRange()
       ],
-      separators: [':', ' ', ' ', ':'],
-      titles: ['Start', ' ', 'Finish'],
+      initIdxs: [0, ...this.getInitIdxs()],
+      separators: [' | ', ':', ' ', ' ', ':'],
+      titles: [' ', 'Start', ' ', 'Finish'],
     );
   }
 }

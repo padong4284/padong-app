8351e10c0da64cf31105e47c8e7e77549f6d0eeb
JTJun <tae7130@snu.ac.kr>
2021-02-20 12:31:51 +0900

    feat: building card basement

---
+++ lib/ui/widgets/cards/building_card.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:padong/ui/theme/app_theme.dart';
+import 'package:padong/ui/widgets/buttons/bottom_buttons.dart';
+
+Map<String, String> getNode(String id) {
+  return {'title': 'Title' + id, 'description': "It's sample description"};
+}
+
+class BuildingCard extends StatelessWidget {
+  final String _id; // node's _id
+  final Map<String, String> node;
+
+  BuildingCard(id)
+      : this.node = getNode(id),
+        this._id = id;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+        onTap: () {},
+        // TODO: Routing to Post
+        child: ConstrainedBox(
+            constraints: BoxConstraints(maxWidth: 255, maxHeight: 140),
+            child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
+                ),
+                elevation: 3.0,
+                margin: const EdgeInsets.all(5),
+                child: Row(children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: AppTheme.colors.lightSupport,
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(5),
+                            bottomLeft: Radius.circular(5))),
+                    width: 140,
+                    height: 140,
+                  ),
+                  Column(children: [
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.only(
+                            top: 10.0, left: 10.0, right: 10.0),
+                        child: Text(this.node['title'],
+                            style: AppTheme.getFont(
+                                color: AppTheme.colors.fontPalette[2],
+                                isBold: true))),
+                    Container(
+                        alignment: Alignment.centerLeft,
+                        margin: const EdgeInsets.only(left: 10.0, right: 10.0),
+                        child: Text('Summary', // TODO: this.description2summary
+                            style: AppTheme.getFont(
+                                color: AppTheme.colors.fontPalette[3]))),
+                    //BottomButtons(bottoms: [0, null, 0]),
+                  ])
+                ]))));
+  }
+}

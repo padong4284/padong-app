38637e8b93268e83463e8841d2f87364260370a1
JTJun <tae7130@snu.ac.kr>
2021-02-20 13:18:46 +0900

    refactor: extends post card

--- lib/ui/widgets/cards/building_card.dart
+++ lib/ui/widgets/cards/building_card.dart
@@ -1,70 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:padong/ui/theme/app_theme.dart';
 import 'package:padong/ui/widgets/buttons/bottom_buttons.dart';
+import 'package:padong/ui/widgets/cards/post_card.dart';
 
-Map<String, String> getNode(String id) {
-  return {'title': 'Title' + id, 'description': "It's sample description"};
-}
-
-class BuildingCard extends StatelessWidget {
-  final String _id; // node's _id
-  final Map<String, String> node;
-
-  BuildingCard(id)
-      : this.node = getNode(id),
-        this._id = id;
+class BuildingCard extends PostCard {
+  BuildingCard(id) : super(id);
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-        onTap: () {},
-        // TODO: Routing to Post
-        child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 255, maxHeight: 140),
-            child: Card(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(5.0),
-                ),
-                elevation: 3.0,
-                margin: const EdgeInsets.all(5),
-                child: Row(children: [
-                  Container(
-                    decoration: BoxDecoration(
-                        color: AppTheme.colors.lightSupport,
-                        borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(5),
-                            bottomLeft: Radius.circular(5))),
-                    width: 140,
-                    height: 140,
-                  ),
-                  Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Container(
-                            width: 105,
-                            height: 90,
-                            padding: const EdgeInsets.symmetric(
-                                horizontal: 10, vertical: 10),
-                            child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(this.node['title'],
-                                      overflow: TextOverflow.ellipsis,
-                                      style: AppTheme.getFont(
-                                          color: AppTheme.colors.fontPalette[2],
-                                          isBold: true)),
-                                  Text(this.node['description'],
-                                      overflow: TextOverflow.ellipsis,
-                                      style: AppTheme.getFont(
-                                        color: AppTheme.colors.fontPalette[3],
-                                      ))
-                                ])),
-                        Container(
-                            width: 100,
-                            margin: const EdgeInsets.only(bottom: 10),
-                            child: BottomButtons(bottoms: [0, null, 0])),
-                      ])
-                ]))));
+    return this.baseCard(
+        width: 255,
+        height: 140,
+        child: Row(children: [
+          this.pictureArea(isRoate: true, height: 140),
+          Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                    width: 105,
+                    height: 90,
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 10, vertical: 10),
+                    child: this.titleArea()),
+                Container(
+                    width: 100,
+                    margin: const EdgeInsets.only(bottom: 10),
+                    child: BottomButtons(bottoms: [0, null, 0])),
+              ])
+        ]));
   }
 }
--- lib/ui/widgets/cards/post_card.dart
+++ lib/ui/widgets/cards/post_card.dart
@@ -16,45 +16,56 @@ class PostCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return this.baseCard(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        this.pictureArea(),
+        Container(
+            padding: const EdgeInsets.only(top: 10, right: 10, left: 10),
+            child: this.titleArea()),
+        BottomButtons(bottoms: [0, null, 0]),
+      ]),
+    );
+  }
+
+  Widget baseCard(
+      {@required Widget child, double width = 140, double height = 220}) {
     return InkWell(
-        onTap: () {},
-        // TODO: Routing to Post
+        onTap: () {
+          // TODO: Routing to Post by id
+        },
         child: ConstrainedBox(
-            constraints: BoxConstraints(maxWidth: 140, maxHeight: 220),
+            constraints: BoxConstraints(maxWidth: width, maxHeight: height),
             child: Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              ),
-              elevation: 3.0,
-              margin: const EdgeInsets.all(5),
-              child: Column(children: [
-                Container(
-                  decoration: BoxDecoration(
-                      color: AppTheme.colors.lightSupport,
-                      borderRadius: BorderRadius.only(
-                          topLeft: Radius.circular(5),
-                          topRight: Radius.circular(5))),
-                  width: 140,
-                  height: 130,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0),
                 ),
-                Container(
-                  alignment: Alignment.topLeft,
-                    margin: const EdgeInsets.only(top: 10, right: 10, left: 10),
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(this.node['title'],
-                              overflow: TextOverflow.ellipsis,
-                              style: AppTheme.getFont(
-                                  color: AppTheme.colors.fontPalette[2],
-                                  isBold: true)),
-                          Text(this.node['description'],
-                              overflow: TextOverflow.ellipsis,
-                              style: AppTheme.getFont(
-                                  color: AppTheme.colors.fontPalette[3]))
-                        ])),
-                BottomButtons(bottoms: [0, null, 0]),
-              ]),
-            )));
+                elevation: 3.0,
+                margin: const EdgeInsets.all(5),
+                child: child)));
+  }
+
+  Widget titleArea() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text(this.node['title'],
+          overflow: TextOverflow.ellipsis,
+          style: AppTheme.getFont(
+              color: AppTheme.colors.fontPalette[2], isBold: true)),
+      Text(this.node['description'],
+          overflow: TextOverflow.ellipsis,
+          style: AppTheme.getFont(color: AppTheme.colors.fontPalette[3]))
+    ]);
+  }
+
+  Widget pictureArea({bool isRoate = false, double height = 130}) {
+    return Container(
+      decoration: BoxDecoration(
+          color: AppTheme.colors.lightSupport,
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(5),
+              topRight: Radius.circular(isRoate ? 0 : 5),
+              bottomLeft: Radius.circular(isRoate ? 5 : 0))),
+      width: 140,
+      height: height,
+    );
   }
 }

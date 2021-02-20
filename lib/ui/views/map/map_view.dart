b36ab0966d9974bcb482016c1949b293d9bad008
JTJun <35912840+jtjun@users.noreply.github.com>
2021-02-20 13:45:40 +0900

    feat: Building Card (#125)
    * refactor: TimeCard -> TimelineCard
    
    * feat: building card basement
    
    * refactor: cards used at timeline
    
    * feat: Buidling Card
    
    * refactor: extends post card

--- lib/ui/views/map/map_view.dart
+++ lib/ui/views/map/map_view.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:padong/ui/widgets/cards/building_card.dart';
 import 'package:padong/ui/widgets/safe_padding_template.dart';
 
 class MapView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafePaddingTemplate(
-      children: [Text('Map View')]
+      children: [
+        BuildingCard('1234')
+      ]
     );
   }
 }

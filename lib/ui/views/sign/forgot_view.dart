6cfbc74516a5336c133519b7458c045a81e2e69f
JTJun <35912840+jtjun@users.noreply.github.com>
2021-02-20 15:32:05 +0900

    feat: Chat Balloon (#127)
    * refactor: minor dart analysis warnings
    
    * feat: message box with timestamp
    
    * feat: ChatBalloon

--- lib/ui/views/sign/forgot_view.dart
+++ lib/ui/views/sign/forgot_view.dart
@@ -1,36 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:padong/ui/shared/dialog_callback.dart';
-import 'package:padong/ui/widgets/buttons/floating_bottom_button.dart';
-import 'package:padong/ui/widgets/buttons/switch_button.dart';
-import 'package:padong/ui/widgets/buttons/toggle_icon_button.dart';
-import 'package:padong/ui/widgets/inputs/times/date_time_range_picker.dart';
+import 'package:padong/ui/shared/types.dart';
+import 'package:padong/ui/widgets/inputs/bottom_sender.dart';
 import 'package:padong/ui/widgets/safe_padding_template.dart';
 import 'package:padong/ui/widgets/bars/back_app_bar.dart';
+import 'package:padong/ui/widgets/tiles/chat_balloon.dart';
 
 class ForgotView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafePaddingTemplate(
-      appBar: BackAppBar(
-          switchButton: SwitchButton(
-            options: ['write', 'prev'],
-          ),
-          actions: [
-            ToggleIconButton(
-                defaultIcon: Icons.favorite_outline_rounded,
-                toggleIcon: Icons.favorite_rounded),
-            ToggleIconButton(
-              defaultIcon: Icons.bookmark_outline_rounded,
-              toggleIcon: Icons.bookmark_rounded,
-              onPressed:
-                  dialogCallback(context, 'Bookmark', 'Add page to bookmarks!'),
-            )
-          ]),
-      children: [
-        DateTimeRangePicker(minuteGap: 5),
-      ],
-      floatingBottomBar: (isScrollingDown) => FloatingBottomButton(
-          title: 'write', onTap: () {}, isScrollingDown: isScrollingDown),
-    );
+        appBar: BackAppBar(title: 'tae7130'),
+        children: [
+          ChatBalloon('0123'),
+        ],
+        floatingBottomBar: (_) =>
+            BottomSender(BottomSenderType.CHAT));
   }
 }
---
+++ lib/ui/widgets/tiles/chat_balloon.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:padong/ui/theme/app_theme.dart';
+import 'package:padong/ui/widgets/buttons/user_profile_button.dart';
+
+Map<String, dynamic> getMessageAPI(String id) {
+  return {
+    'ownerId': '0321',
+    'owner': getUserAPI('0321'),
+    'createdAt': DateTime.now(),
+    'message':
+        'this is a chat balloon, U can chat with your friends.\nGood luck!'
+  };
+}
+
+class ChatBalloon extends StatelessWidget {
+  final String _id;
+  final bool hideTimestamp;
+  final bool hideSender;
+  final Map<String, dynamic> chatMsg;
+
+  ChatBalloon(id, {this.hideTimestamp = false, this.hideSender = false})
+      : this._id = id,
+        this.chatMsg = getMessageAPI(id);
+
+  @override
+  Widget build(BuildContext context) {
+    bool isMine = false; // TODO: check owner == me
+    List<Widget> message = [this.messageBox(isMine)];
+    if (!this.hideTimestamp) message.add(this.timestamp(isMine));
+    return Container(
+        child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment:
+                isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+            children: [
+          isMine ? SizedBox.shrink() : this.sender(),
+          Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+            isMine || this.hideSender
+                ? SizedBox.shrink()
+                : Text(
+                    this.chatMsg['owner']['username'],
+                    style:
+                        AppTheme.getFont(color: AppTheme.colors.fontPalette[1]),
+                  ),
+            Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [...(isMine ? message.reversed : message)])
+          ])
+        ]));
+  }
+
+  Widget messageBox(bool isMine) {
+    return ConstrainedBox(
+        constraints: BoxConstraints(maxWidth: 218),
+        child: Container(
+            decoration: BoxDecoration(
+                color: isMine
+                    ? AppTheme.colors.primary
+                    : AppTheme.colors.lightSupport,
+                borderRadius: const BorderRadius.all(Radius.circular(5.0))),
+            margin: const EdgeInsets.symmetric(vertical: 3),
+            padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 13),
+            child: Text(
+              this.chatMsg['message'],
+              style: AppTheme.getFont(
+                  color: isMine
+                      ? AppTheme.colors.fontPalette[4]
+                      : AppTheme.colors.fontPalette[1]),
+            )));
+  }
+
+  Widget timestamp(bool isMine) {
+    String hNm =
+        this.chatMsg['createdAt'].toString().split(' ')[1].substring(0, 5);
+    return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 6, vertical: 3),
+        child: Text(hNm,
+            style: AppTheme.getFont(
+                color: isMine
+                    ? AppTheme.colors.semiPrimary
+                    : AppTheme.colors.fontPalette[3],
+                fontSize: AppTheme.fontSizes.small)));
+  }
+
+  Widget sender() {
+    return Padding(
+        padding: const EdgeInsets.only(top: 5, right: 10),
+        child: this.hideSender
+            ? SizedBox(width: 40)
+            : UserProfileButton(
+                username: this.chatMsg['owner']['username'], size: 40));
+  }
+}

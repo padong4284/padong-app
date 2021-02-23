9e45f3de17d62b5d33273dd969239c3083b90b40
jtjun <tae7130@snu.ac.kr>
2021-02-23 23:59:54 +0900

    feat: google map setting

--- lib/ui/views/map/map_view.dart
+++ lib/ui/views/map/map_view.dart
@@ -1,14 +1,46 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:padong/ui/widgets/cards/building_card.dart';
-import 'package:padong/ui/widgets/safe_padding_template.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapView extends StatelessWidget {
+class MapView extends StatefulWidget {
+  @override
+  State<MapView> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapView> {
+  Completer<GoogleMapController> _controller = Completer();
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static final CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414);
+
   @override
   Widget build(BuildContext context) {
-    return SafePaddingTemplate(
-      children: [
-        BuildingCard('1234')
-      ]
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheLake,
+        label: Text('To the lake!'),
+        icon: Icon(Icons.directions_boat),
+      ),
     );
+  }
+
+  Future<void> _goToTheLake() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }

93af3ade9fa9e3ae147d3b4a3f375c5b5fa9443c
jtjun <tae7130@snu.ac.kr>
2021-02-24 19:06:13 +0900

    feat: styling basement

--- lib/ui/views/map/map_view.dart
+++ lib/ui/views/map/map_view.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart' show rootBundle;
 
 class MapView extends StatefulWidget {
   @override
@@ -8,39 +9,38 @@ class MapView extends StatefulWidget {
 }
 
 class MapSampleState extends State<MapView> {
+  String _mapStyle;
   Completer<GoogleMapController> _controller = Completer();
 
-  static final CameraPosition _kGooglePlex = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
+  final CameraPosition _georgiaTech = CameraPosition(
+    target: LatLng(33.775992835163144, -84.3962589592725),
     zoom: 14.4746,
+    //bearing: 192.8334901395799,
+    //tilt: 59.440717697143555,
   );
 
-  static final CameraPosition _kLake = CameraPosition(
-      bearing: 192.8334901395799,
-      target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
-      zoom: 19.151926040649414);
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.loadString('assets/map_style.txt').then((string) {
+      this._mapStyle = string;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: _kGooglePlex,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: _goToTheLake,
-        label: Text('To the lake!'),
-        icon: Icon(Icons.directions_boat),
-      ),
+          initialCameraPosition: this._georgiaTech,
+          onMapCreated: (GoogleMapController controller) {
+            controller.setMapStyle(this._mapStyle);
+            _controller.complete(controller);
+          }),
     );
   }
 
-  Future<void> _goToTheLake() async {
+  Future<void> _goToGT() async {
     final GoogleMapController controller = await _controller.future;
-    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
+    controller.animateCamera(CameraUpdate.newCameraPosition(this._georgiaTech));
   }
 }

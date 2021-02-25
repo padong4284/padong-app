c2de9b9ff288e66d422fb8c1adeb9f4ea37c85eb
kim hyun sik <wmf2fkrh@gmail.com>
2021-02-26 00:19:03 +0900

    feat: Add getFriends to User in viewmodels User

--- lib/core/viewmodels/user/user.dart
+++ lib/core/viewmodels/user/user.dart
@@ -38,5 +38,33 @@ class User extends ModelUser {
     throw Exception("UserId doesn't exists");
   }
 
-}
+  /// getFreinds return List<User>
+  Future<List<User>> getFriends() async{
+    List<User> friendList;
 
+    //Maybe it will be refactored later.
+    for (String friendId in this.friendIds) {
+      QuerySnapshot query = await _userDB.ref.where(
+          "id", isEqualTo: friendId).get();
+      if (query.size == 0) {
+        continue;
+      }
+      friendList.add(
+          User.fromMap(query.docs.first.data(), query.docs.first.id));
+    }
+    return friendList;
+  }
+
+/// getParent return ModelUniversity
+//todo : erase remark when Uiversity_view_model is done.
+/*Future<ModelUniversity> getParent(){
+    University university;
+    return university.getUniversityById(this.parentNodeId);
+  }*/
+
+///getPosts return List<Post>
+//todo : erase remark when Post_view_model is done.
+/*List<Post> getPosts() {
+    return Post.getPostsById(this.id);
+  }*/
+}

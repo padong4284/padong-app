fd4626dd5e89f7295eedfaffb7a5dcdd5b5dfad3
JTJun <tae7130@snu.ac.kr>
2021-03-02 23:29:25 +0900

    refactor: notification -> subscribe

--- lib/core/node/common/subscribe.dart
+++ lib/core/node/common/subscribe.dart
@@ -1,7 +1,7 @@
 import 'package:padong/core/node/common/like.dart';
 
 // parent: Board
-class Notification extends Like {
-  Notification.fromMap(String id, Map snapshot) :
+class Subscribe extends Like {
+  Subscribe.fromMap(String id, Map snapshot) :
         super.fromMap(id, snapshot);
 }

d192d660cd9296c2c959dc691cae25f7b354d1f0
JTJun <tae7130@snu.ac.kr>
2021-03-02 23:28:11 +0900

    feat: like, bookmark, notification(alert)

---
+++ lib/core/node/common/bookmark.dart
@@ -0,0 +1,8 @@
+import 'package:padong/core/node/common/like.dart';
+
+// parent: Post
+class Bookmark extends Like {
+  Bookmark.fromMap(String id, Map snapshot) :
+        super.fromMap(id, snapshot);
+}
+
---
+++ lib/core/node/common/notification.dart
@@ -0,0 +1,7 @@
+import 'package:padong/core/node/common/like.dart';
+
+// parent: Board
+class Notification extends Like {
+  Notification.fromMap(String id, Map snapshot) :
+        super.fromMap(id, snapshot);
+}

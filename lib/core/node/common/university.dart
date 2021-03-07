88b0a2ebb5f7a29361d8f7759424502d6b2190dd
Seongbin Hong <vertextoedge@gmail.com>
2021-03-07 22:28:05 +0900

    refactor: add getUniversityByName

--- lib/core/node/common/university.dart
+++ lib/core/node/common/university.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:padong/core/node/title_node.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:padong/core/services/padong_fb.dart';
 
 // parent: PADONG (one and only)
 class University extends TitleNode {
@@ -18,5 +20,16 @@ class University extends TitleNode {
       'emblemImgURL': this.emblemImgURL,
       'location': this.location.toJson(),
     };
+  }
+
+  static Future<University> getUniversityByTitle(String universityName) async {
+    var results = await PadongFB.getNodesByRule(University,
+        rule: (Query q) => (q.where("title", isEqualTo: universityName)),
+    );
+    if (results.isNotEmpty){
+      return results.first;
+    }else{
+      throw Exception("There's no University");
+    }
   }
 }

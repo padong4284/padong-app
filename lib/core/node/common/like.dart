3cd77f14b609b785c10be72caeb62a84fe2d3973
JTJun <tae7130@snu.ac.kr>
2021-03-10 19:28:54 +0900

    fix: Like needs parentType

--- lib/core/node/common/like.dart
+++ lib/core/node/common/like.dart
@@ -2,10 +2,19 @@ import 'package:padong/core/node/node.dart';
 
 // parent: Post, Reply, ReReply
 class Like extends Node {
+  String parentType;
+
   Like();
 
-  Like.fromMap(String id, Map snapshot) : super.fromMap(id, snapshot);
+  Like.fromMap(String id, Map snapshot)
+      : this.parentType = snapshot['parentType'],
+        super.fromMap(id, snapshot);
 
   @override
   generateFromMap(String id, Map snapshot) => Like.fromMap(id, snapshot);
+
+  @override
+  Map<String, dynamic> toJson() {
+    return {'parentType': this.parentType, ...super.toJson()};
+  }
 }

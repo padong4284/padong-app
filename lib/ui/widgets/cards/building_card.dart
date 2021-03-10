33aa31b4002ec17aa62c986ca04732215bf9ad83
JTJun <tae7130@snu.ac.kr>
2021-03-10 20:30:44 +0900

    refactor: building card show description

--- lib/ui/widgets/cards/building_card.dart
+++ lib/ui/widgets/cards/building_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:padong/ui/theme/app_theme.dart';
 import 'package:padong/ui/widgets/buttons/bottom_buttons.dart';
 import 'package:padong/ui/widgets/cards/photo_card.dart';
 
@@ -18,7 +19,6 @@ class BuildingCard extends PhotoCard {
               children: [
                 Container(
                     width: 105,
-                    height: 90,
                     padding: const EdgeInsets.symmetric(
                         horizontal: 10, vertical: 10),
                     child: this.titleArea()),
@@ -28,5 +28,20 @@ class BuildingCard extends PhotoCard {
                     child: BottomButtons(left: 8, bottoms: [0, null, 0])),
               ])
         ]));
+  }
+
+  @override
+  Widget titleArea() {
+    return Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+      Text(this.node['title'],
+          overflow: TextOverflow.ellipsis,
+          style: AppTheme.getFont(
+              color: AppTheme.colors.fontPalette[2], isBold: true)),
+      SizedBox(height: 5),
+      Container(
+          height: 55,
+          child: Text(this.node['description'],
+              style: AppTheme.getFont(color: AppTheme.colors.fontPalette[3])))
+    ]);
   }
 }

383546d0206f6d76c5aac83af7d15384cf77227e
Seongbin Hong <vertextoedge@gmail.com>
2021-03-18 22:32:18 +0900

    refactor: checkHeader return null

--- lib/util/http/http.dart
+++ lib/util/http/http.dart
@@ -1,27 +1,24 @@
 import 'package:http/http.dart' as http;
 import 'package:padong/core/shared/validator.dart';
 
-class CheckUrl {
-  String url;
-  bool checkResult;
-  CheckUrl(this.url, this.checkResult);
-}
-
-Future<CheckUrl> checkHeader(
+Future<String> checkHeader(
     String url, Map<String, String> CompareHeader) async {
   var res = await http.head(Uri.parse(url));
   if (res.statusCode != 200) {
-    return CheckUrl(url, false);
+    return null;
   }
   for (var i in CompareHeader.keys) {
     if (!Validator.isValid(RegExp(CompareHeader[i]), res.headers[i])) {
-      return CheckUrl(url, false);
+      return null;
     }
   }
-  return CheckUrl(url, true);
+  return url;
 }
 
-Future<List<CheckUrl>> checkImgUrls(List<String> urls) async {
+Future<List<String>> checkImgUrls(List<String> urls) async {
   return await Future.wait(
-      urls.map((url) => checkHeader(url, {'Content-Type': "image/.+"})));
+          urls.map((url) => checkHeader(url, {'Content-Type': "image/.+"})))
+      .timeout(Duration(seconds: 15))
+      .then((value) => value.where((element) => element != null).toList());
+
 }

df7d618f5c5d19a909fe0c60181a1b17943c6343
jtjun <tae7130@snu.ac.kr>
2021-04-03 18:48:40 +0900

    feat: stack scroll listener

--- lib/ui/template/board_template.dart
+++ lib/ui/template/board_template.dart
@@ -11,6 +11,7 @@
 import 'package:flutter/material.dart';
 import 'package:padong/core/node/deck/board.dart';
 import 'package:padong/core/node/deck/post.dart';
+import 'package:padong/core/node/schedule/schedule.dart';
 import 'package:padong/core/padong_router.dart';
 import 'package:padong/core/service/session.dart';
 import 'package:padong/core/shared/types.dart';
@@ -35,64 +36,67 @@ class BoardTemplate extends StatelessWidget {
   final Function onPressMore;
   final List<Widget> actions;
   final List<Widget> center;
+  final ScrollController _controller = ScrollController();
 
   BoardTemplate(
-      this.board,
-      this.post,
-      this.setState,
-      {
-        this.writeMessage = 'write',
-        this.postsMessage = 'Posts',
-        this.emptyMessage = 'Please write first Post',
-        this.onPressMore,
-        this.actions,
-        this.center,
-      }): this.access = Session.checkAccess(board);
+    this.board,
+    this.post,
+    this.setState, {
+    this.writeMessage = 'write',
+    this.postsMessage = 'Posts',
+    this.emptyMessage = 'Please write first Post',
+    this.onPressMore,
+    this.actions,
+    this.center,
+  }) : this.access = Session.checkAccess(board);
 
   @override
   Widget build(BuildContext context) {
     return SafePaddingTemplate(
-        floatingActionButtonGenerator: (isScrollingDown) =>
-            PadongButton(isScrollingDown: isScrollingDown, bottomPadding: 40),
-        floatingBottomBarGenerator: this.access == ACCESS.READWRITE
-            ? (isScrollingDown) => FloatingBottomButton(
-            title: this.writeMessage[0].toUpperCase()
-                + this.writeMessage.substring(1).toLowerCase(),
-            onTap: () { // register refresh to update
-              PadongRouter.refresh = () => this.setState(() {});
-              PadongRouter.routeURL(
-                  '${this.writeMessage.toLowerCase()}?id=${
-                      this.board.id}&type=${this.board.type}',
-                  this.board);
-            },
-            isScrollingDown: isScrollingDown)
-            : null,
-        appBar: BackAppBar(title: this.board.title, actions: [
-          IconButton(
-              icon: Icon(Icons.more_horiz, color: AppTheme.colors.support),
-              onPressed: this.onPressMore ?? () {}),
-          ...(this.actions ?? [])
-        ]),
-        children: [],
-        stackChildren: [
-          Padding(
+      floatingActionButtonGenerator: (isScrollingDown) =>
+          PadongButton(isScrollingDown: isScrollingDown, bottomPadding: 40),
+      floatingBottomBarGenerator: this.access == ACCESS.READWRITE
+          ? (isScrollingDown) => FloatingBottomButton(
+              title: this.writeMessage[0].toUpperCase() +
+                  this.writeMessage.substring(1).toLowerCase(),
+              onTap: () {
+                // register refresh to update
+                PadongRouter.refresh = () => this.setState(() {});
+                PadongRouter.routeURL(
+                    '${this.writeMessage.toLowerCase()}?id=${this.board.id}&type=${this.board.type}',
+                    this.board);
+              },
+              isScrollingDown: isScrollingDown)
+          : null,
+      appBar: BackAppBar(title: this.board.title, actions: [
+        IconButton(
+            icon: Icon(Icons.more_horiz, color: AppTheme.colors.support),
+            onPressed: this.onPressMore ?? () {}),
+        ...(this.actions ?? [])
+      ]),
+      children: [],
+      stackChildren: [
+        Padding(
             padding: const EdgeInsets.symmetric(
-              horizontal: AppTheme.horizontalPadding),
-            child:
-          InfinityScroller(
+                horizontal: AppTheme.horizontalPadding),
+            child: InfinityScroller(
               this.board,
               this.post,
               builder: (post) => PostTile(post as Post),
               emptyMessage: this.emptyMessage,
               preWidgets: [
                 Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 10.0),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 5, vertical: 10.0),
                   child: PadongMarkdown(this.board.description),
                 ),
                 ...(this.center ?? []),
                 TitleHeader(this.postsMessage)
               ],
-          )),
-        ]);
+              scrollController: this._controller,
+            ))
+      ],
+      stackScrollController: this._controller,
+    );
   }
 }
--- lib/ui/widget/container/infinity_scroller.dart
+++ lib/ui/widget/container/infinity_scroller.dart
@@ -9,12 +9,16 @@ class InfinityScroller extends StatefulWidget {
   final int itemPerPage;
   final String emptyMessage;
   final List<Widget> preWidgets;
+  final ScrollController scrollController;
 
-  InfinityScroller(this.node, this.child, {
+  InfinityScroller(
+    this.node,
+    this.child, {
     @required this.builder,
     this.itemPerPage = 20,
     this.emptyMessage = 'Nothing to Show You',
     this.preWidgets,
+    this.scrollController,
   });
 
   @override
@@ -37,72 +41,77 @@ class _InfinityScrollerState extends State<InfinityScroller> {
 
   @override
   Widget build(BuildContext context) {
-    if (this._children.isEmpty){
+    if (this._children.isEmpty) {
       return Column(children: [
         ...(widget.preWidgets ?? []),
-        this._isLoading ? this._loadingArea()
-            : (this._isError ? this._retryArea()
-              : NoDataMessage(widget.emptyMessage, height: 100))
+        this._isLoading
+            ? this._loadingArea()
+            : (this._isError
+                ? this._retryArea()
+                : NoDataMessage(widget.emptyMessage, height: 100))
       ]);
-    } else return ListView.builder(
-      scrollDirection: Axis.vertical,
-      itemCount: (widget.preWidgets != null ? 1 : 0)
-          + this._children.length + (this._hasMore ? 1: 0),
-      itemBuilder: (context, index) {
-        if (this._hasMore && (index == this._children.length - this._nextPageFlag))
-          this.fetchChildren();
-        if (widget.preWidgets != null && index == 0)
-          return Column(children: [...widget.preWidgets]);
-        else if (index == this._children.length) {
-          if (this._isError) return this._retryArea();
-          else if(this._hasMore) return this._loadingArea();
-          else return SizedBox(height: 50);
-        }
-        return widget.builder(this._children[index]);
-      }
-    );
+    } else
+      return ListView.builder(
+          scrollDirection: Axis.vertical,
+          controller: widget.scrollController,
+          itemCount: 1 + this._children.length + (this._hasMore ? 1 : 0),
+          itemBuilder: (context, index) {
+            if (this._hasMore &&
+                (index == this._children.length - this._nextPageFlag))
+              this.fetchChildren();
+            if (index == 0)
+              return Column(children: [...(widget.preWidgets ?? [])]);
+            else if (index == this._children.length) {
+              if (this._isError)
+                return this._retryArea();
+              else if (this._hasMore)
+                return this._loadingArea();
+              else
+                return SizedBox(height: 50);
+            }
+            return widget.builder(this._children[index]);
+          });
   }
 
   Widget _loadingArea() {
     return Center(
-        child: Padding(padding:  const EdgeInsets.all(15),
-          child: CircularProgressIndicator(),)
-    );
+        child: Padding(
+      padding: const EdgeInsets.all(15),
+      child: CircularProgressIndicator(),
+    ));
   }
 
   Widget _retryArea() {
     return Center(
-        child: InkWell(onTap: () {
-          this._isLoading = true;
-          this.fetchChildren();
-        },
-            child: Padding(padding: const EdgeInsets.all(15),
-                child: Text('Error while loading.\nTap to Retry'))
-        )
-    );
+        child: InkWell(
+            onTap: () {
+              this._isLoading = true;
+              this.fetchChildren();
+            },
+            child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text('Error while loading.\nTap to Retry'))));
   }
 
   Future<void> fetchChildren() async {
     print('fetch ${this._children.length}');
     try {
-      List<Node> children = await widget.node.getChildren(
-          widget.child,
-          limit: widget.itemPerPage,
-          startAt: this._lastChild,
-          upToDate: true);
-      if(mounted) setState(() {
-        this._hasMore = children.length == widget.itemPerPage;
-        this._isLoading = false;
-        this._lastChild = children.isNotEmpty
-                            ? children[children.length - 1]
-                            : null;
-        this._children.addAll(children);
-      });
+      List<Node> children = await widget.node.getChildren(widget.child,
+          limit: widget.itemPerPage, startAt: this._lastChild, upToDate: true);
+      if (mounted)
+        setState(() {
+          this._hasMore = children.length == widget.itemPerPage;
+          this._isLoading = false;
+          this._lastChild =
+              children.isNotEmpty ? children[children.length - 1] : null;
+          this._children.addAll(children);
+        });
     } catch (e) {
-      if(mounted) setState(() {
-        this._isError = true;
-        this._isLoading = false;
-      });
+      if (mounted)
+        setState(() {
+          this._isError = true;
+          this._isLoading = false;
+        });
     }
   }
 }

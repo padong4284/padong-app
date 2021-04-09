96296bcf47ada5909b808f766170063e2c799620
Seongbin Hong <vertextoedge@gmail.com>
2021-04-09 19:16:04 +0900

    fix: check univ.domains is null

--- lib/core/shared/validator.dart
+++ lib/core/shared/validator.dart
@@ -46,7 +46,7 @@ class Validator {
   static bool universityEmailVerification(University univ, String email) {
     if (!isValid(emailRule, email)) return false;
     if (univ.title == "PADONG") return true;
-    assert(univ != null);
+    assert(univ.domains != null);
     bool result = univ.domains.any((e) => Validator.isValid(RegExp(e), email));
     return result;
   }

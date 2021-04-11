2b1c50a94483d0404086126d59608e9633bc30a8
jtjun <tae7130@snu.ac.kr>
2021-04-11 16:49:29 +0900

    refactor: refresh stack

--- lib/core/padong_router.dart
+++ lib/core/padong_router.dart
@@ -43,8 +43,18 @@ import 'package:padong/ui/view/sign/sign_up_view.dart';
 import 'package:padong/util/animation_routers.dart';
 
 class PadongRouter {
+  static List<Function()> _refreshes;
+
   static BuildContext context;
-  static Function() refresh;
+
+  static Function() get refresh => _refreshes.removeLast();
+
+  static set refresh(Function() callback) {
+    if (callback == null)
+      _refreshes.removeLast();
+    else
+      _refreshes.add(callback);
+  }
 
   static Route<dynamic> generateRoute(RouteSettings settings) {
     final Map<String, dynamic> args =
@@ -127,8 +137,9 @@ class PadongRouter {
             pageBuilder: (_, __, ___) => ChatRoomView(args['node']),
             direction: 2);
       case '/chat':
-        return slideRouter(pageBuilder: (_, __, ___) => ChatView(
-            args['node'], isDirectTo: args['isDirectTo'] != null));
+        return slideRouter(
+            pageBuilder: (_, __, ___) =>
+                ChatView(args['node'], isDirectTo: args['isDirectTo'] != null));
 
       case '/profile':
         return slideRouter(
